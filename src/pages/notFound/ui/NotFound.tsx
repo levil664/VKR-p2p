bd8f85148286aff2ccb68545686de29da2b8c8ab
lev tarasov <56757711+levil664@users.noreply.github.com>
2025-03-19 23:15:43 +0500

    auth page + fsd integration

--- src/pages/notFound/ui/NotFound.tsx
+++ src/pages/notFound/ui/NotFound.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 export const NotFound: React.FC = () => {
-    return (
-        <Typography variant="h4" align="center">
-            Страница не найдена
-        </Typography>
-    );
+  return (
+    <Typography variant="h4" align="center">
+      Страница не найдена
+    </Typography>
+  );
 };
